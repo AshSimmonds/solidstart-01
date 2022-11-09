656afbb072c8cc37929dcf08768541bd49be4849
Ash Simmonds <ashsimmonds@gmail.com>
2022-11-10 09:21:25 +1030

    test out server function

--- src/routes/opps.tsx
+++ src/routes/opps.tsx
@@ -1,17 +1,25 @@
-import { A } from "solid-start";
 
-export default function BlankPage() {
+// console.log(`opps.tsx PLAIN: ${import.meta.env.VITE_AIRTABLE_API_KEY}`)
+
+import server$ from "solid-start/server";
+
+const logHello = server$(async (message: string) => {
+    console.log(message)
+})
+
+logHello("Hello from the FRONTMATTER")
+
+
+export default function OverseasPayloadPermitListPage() {
+
+    logHello("Hello from the FUNCTION")
+
     return (
         <main class="text-center mx-auto ">
             <h1 >
-                Empty
+                Overseas Payload Permits
             </h1>
 
-            <h2>Still blank</h2>
-
-            <div>
-                nothing to see here
-            </div>
 
         </main>
     );
