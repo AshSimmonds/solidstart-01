54bec1fbc710aa443f5036cc29280ec3935fde1d
Ash Simmonds <ashsimmonds@gmail.com>
2022-11-10 16:16:59 +1030

    add vercel adapter for reasons

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import solid from "solid-start/vite";
 import { defineConfig } from "vite";
+import vercel from "solid-start-vercel";
 
 export default defineConfig({
-    plugins: [solid()],
+    plugins: [
+        solid({ adapter: vercel() })
+    ],
 });
