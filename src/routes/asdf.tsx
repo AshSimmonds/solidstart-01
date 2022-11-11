cc4fb591c01a57949226432665e11c05a6059052
Ash Simmonds <ashsimmonds@gmail.com>
2022-11-12 05:10:52 +1030

    scaffold out asdf page

---
+++ src/routes/asdf.tsx
@@ -0,0 +1,18 @@
+import { A } from "solid-start";
+
+export default function AsdfPage() {
+    return (
+        <div>
+            <h1 >
+                asdf
+            </h1>
+
+            <h2>asdf</h2>
+
+            <div>
+                asdf
+            </div>
+
+        </div>
+    );
+}
