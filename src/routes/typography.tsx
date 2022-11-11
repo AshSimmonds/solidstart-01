14c535360fa6e18a85952a52ea54f323a0e7af94
Ash Simmonds <ashsimmonds@gmail.com>
2022-11-12 05:11:48 +1030

    Create typography.tsx

---
+++ src/routes/typography.tsx
@@ -0,0 +1,52 @@
+import { A } from "solid-start";
+
+export default function TypographyIndexPage() {
+    return (
+        <div>
+            <h1>Typography and styling resources</h1>
+
+            <h2>How stuff looks on this site</h2>
+
+            <div class="btn-group">
+                <a href="/typography-html" class="btn btn-outline">STANDARD HTML</a>
+
+                <a href="/typography-markdown" class="btn btn-outline">MARKDOWN</a>
+
+                <a href="/typography-tailwind" class="btn btn-outline"
+                >TAILWIND COMPONENTS</a
+                >
+            </div>
+
+            <h2>External resources</h2>
+
+            <div class="btn-group btn-group-vertical">
+                <a
+                    href="https://docs.astro.build/en/core-concepts/project-structure/"
+                    class="btn btn-outline">Astro (framework docs)</a
+                >
+
+                <a
+                    href="https://tailwindcss.com/docs/configuration"
+                    class="btn btn-outline">Tailwind (CSS engine)</a
+                >
+
+                <a href="https://daisyui.com/components/" class="btn btn-outline"
+                >DaisyUI (components & theming)</a
+                >
+
+                <a href="https://themes.ionevolve.com" class="btn btn-outline"
+                >IonEvolve (theme builder)</a
+                >
+
+                <a href="https://www.hyperui.dev/" class="btn btn-outline"
+                >HyperUI (components)</a
+                >
+
+                <a href="https://shuffle.dev/editor" class="btn btn-outline"
+                >Shuffle (whole page/site builder)</a
+                >
+            </div>
+        </div>
+
+    )
+}
