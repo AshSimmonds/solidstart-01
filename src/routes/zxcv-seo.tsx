ee37a704113c946cec3f20a36dde313956158a87
Ash Simmonds <ashsimmonds@gmail.com>
2022-11-14 08:28:52 +1030

    Create zxcv-seo.tsx

---
+++ src/routes/zxcv-seo.tsx
@@ -0,0 +1,14 @@
+import { A } from "solid-start";
+import PageTitle from "~/components/PageTitle"
+
+export default function SEOTestPage() {
+    return (
+        <>
+            <PageTitle>SEO test</PageTitle>
+            <h1 >
+                SEO test
+            </h1>
+
+        </>
+    )
+}
